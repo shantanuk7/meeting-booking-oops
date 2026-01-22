67ef910fd24a12e20ec2ecf68fec08f3c8e4b8b7
shantanuk7 <shantanukulkarni365@gmail.com>
2026-01-22 09:36:04 +0530

    Modify a member variable to make it private

--- MeetingRoom.py
+++ MeetingRoom.py
@@ -1,16 +1,16 @@
 class MeetingRoom:
     def __init__(self, name):
         self.name = name
-        self.slots_booked = []
+        self.__slots_booked = []
 
     def is_available(self, slot):
-        if slot in self.slots_booked:
+        if slot in self.__slots_booked:
             return False
         return True
 
     def book_room(self, slot):
         if self.is_available(slot):
-            self.slots_booked.append(slot)
+            self.__slots_booked.append(slot)
             return True
         
         return False
